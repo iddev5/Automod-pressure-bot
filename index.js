80ecd1fbe00a399fe5c0c2df864ef186f076b8a8
iddev5 <ayushbardhan2001@gmail.com>
2020-07-30 00:01:23 +0530

    First attempt at pressure system, still has lot of bugs and quite unbalanced

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ var db = new JsonDB(new ConfigDB("db", true, true, "/"));
 
 // Components in config.js:
 // 1) token
+// 2) cooldown
 const config = require("./config.js");
+
+const cooldown = config.cooldown || 7000;
+const pointWrn = config.pointWrn || 7;
 
 client.on("ready", () => {
 	console.log("Bot for testing automod pressure");
@@ -18,27 +22,42 @@ client.on("message", message => {
 	
 	// Check if the user's entry exits in database
 	// If not then create it.
-	var lastMsg = 0;
-	try { lastMsg = db.getData("/" + message.author.id + "/lastMsg"); }
+	var lastWrn = 0;
+	try { lastWrn = db.getData("/" + message.author.id + "/lastWrn"); }
 	catch { setupUser(message); }
 	
 	const timeNow = (new Date).getTime();
 	
-	if(lastMsg != 0 && timeNow - lastMsg > 700) {
-		// When was the user last warned?
-		const lastWrn = db.getData("/" + message.author.id + "/lastWrn");
-		
-		// Warn only if last warn was longer than 7 seconds ago
-		// else we are just spam warning.
-		if(timeNow - lastWrn > 7000) {
-			message.reply("You are spamming too much");
-			
-			// Update last warn timing
-			db.push("/" + message.author.id + "/lastWrn", timeNow); 
-		}
+	var lastPoints = db.getData("/" + message.author.id + "/points");
+	var lastMsg = db.getData("/" + message.author.id + "/lastMsg");
+	
+	const timeDiff = timeNow - lastMsg;
+	
+	if(timeDiff <= 700) {
+		lastPoints += 2;
+	}
+	else {
+		lastPoints -= (0.5 * timeDiff / 1000);
 	}
 	
-	// Update last message timing
+	if(timeNow - lastWrn > cooldown) {
+		if(lastPoints == config.pointWrn) {
+			// Warn only if last warn was longer than 7 seconds ago
+			// else we are just spam warning.
+			message.reply("You are spamming too much");
+		}
+		else if(lastPoints >= config.pointWrn + 4) {
+			// Mute as the last resort.
+			message.reply("You are getting MUTED");
+		}
+		
+		// Update last warn timing
+		db.push("/" + message.author.id + "/lastWrn", timeNow); 
+		lastPoints -= 2; 
+	}
+	
+	// Update the points
+	db.push("/" + message.author.id + "/points", Math.max(0, lastPoints++)); console.log(lastPoints);
 	db.push("/" + message.author.id + "/lastMsg", timeNow); 
 });
 
@@ -46,7 +65,7 @@ client.login(config.token);
 
 // Setup the user
 function setupUser(message) {
+	db.push("/" + message.author.id + "/points", 0);
 	db.push("/" + message.author.id + "/lastWrn", -1); 
 	db.push("/" + message.author.id + "/lastMsg", -1); 
-	
 }
